3a14e143a9d5c93803bc60cc5f005c158c769c0e
Surya Vengadesan <spvengat@berkeley.edu>
2019-10-31 13:49:59 -0700

    google drive folder download

--- transformer/pretrained_model.sh
+++ transformer/pretrained_model.sh
@@ -3,3 +3,7 @@
 
 #echo "Downloading pretrained model weights"
 
+#https://drive.google.com/drive/folders/192hEuxQxdJEfEFE0MMsVdnPYxPQrjtOW
+#wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=FILEID' -O FILENAME
+
+wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=192hEuxQxdJEfEFE0MMsVdnPYxPQrjtOW' -O output
